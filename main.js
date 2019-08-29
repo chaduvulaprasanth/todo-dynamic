74db96126c30daedcc98c1365ccfb00f8606cc58
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2019-08-29 22:12:17 +0530

    updated todo dynamic

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
-// function keyCode(event) {
-// 		  	var y = event.keyCode;
-// 		  	if (y == 13) {
-// 			var item = document.getElementById("todoInput").value;
-// 			var text = document.createTextNode(item);
-// 			var newItem = document.createElement("li");
-// 			newItem.appenChild(text);
-// 			document.getElementById("todoList").appenChild(newItem);
-// 			}
-// 			document.getElementById("todoInput").addEventListener("keyup",keyCode)
+document.body.style.backgroundColor = "lightgrey";
+var data = [];
+function addtodo(event){
+if(event.keyCode == 13){
+ data.push(event.target.value);
+display()
+}
+}
+function display(){
+var ul = document.querySelector("ul");
+var li = document.createElement("li");
+var check = document.createElement("input");
+check.type = "checkbox";
+var p = document.createElement("p");
+for (let key in data){
+p.textContent = data[key];
+}
+var del = document.createElement("button");
+del.innerText = "x";
+ul.appendChild(li);
+li.appendChild(check);
+li.appendChild(p);
+li.appendChild(del);
+document.querySelector("input").value = "";
+}
+
+document.querySelector("input").addEventListener("keyup",addtodo)
