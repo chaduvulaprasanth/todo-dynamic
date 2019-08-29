e0a0659adb96ed3f07e933a2212937968e07dd33
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2019-08-29 21:49:11 +0530

    intiated todo dynamic

---
+++ main.js
@@ -0,0 +1,10 @@
+// function keyCode(event) {
+// 		  	var y = event.keyCode;
+// 		  	if (y == 13) {
+// 			var item = document.getElementById("todoInput").value;
+// 			var text = document.createTextNode(item);
+// 			var newItem = document.createElement("li");
+// 			newItem.appenChild(text);
+// 			document.getElementById("todoList").appenChild(newItem);
+// 			}
+// 			document.getElementById("todoInput").addEventListener("keyup",keyCode)
